567c5282928b1ba7b49ab89311a23bc0e6ef3873
Kevin Wang <kev.wang424@gmail.com>
2016-11-18 04:58:47 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
+require 'byebug'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    answer = []
+    #debugger
+    self.each do |key,value|
+      arguments.each do |arg|
+        answer << key if arg == value
+      end
+    end
+    answer
   end
 end
